371998567ec87489640c450bb1e6e02aa51c2269
sisoares <simone.santos18@fatec.sp.gov.br>
2019-08-03 14:39:41 -0300

    Correção que não foi corrigida

--- src/main/java/com/testService/TestController.java
+++ src/main/java/com/testService/TestController.java
@@ -1,15 +1,22 @@
+
+
 package com.testService;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+/*
+* Exemplo básico de um comentário em JavaDoc com mais de uma linha.
+* @return teste teste teste.
+*/
+@RestController /*
+* Exemplo básico de um comentário em JavaDoc com mais de uma linha.
+* @return teste teste teste.
+*/
 @CrossOrigin
 public class TestController {
-
-	@GetMapping("/home")
-	public String homeRequest() {
+@GetMapping("/home")
+		public String homeRequest() {
 		return "Hello World!";
 	}
 }
--- src/main/java/com/testService/TestServiceApplication.java
+++ src/main/java/com/testService/TestServiceApplication.java
@@ -1,18 +1,30 @@
 package com.testService;
 
-import com.testService.TestController;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
+/*
+* Exemplo básico de um comentário em JavaDoc com mais de uma linha.
+* teste teste teste.
+*/
 @SpringBootApplication
+/*
+* Exemplo básico de um comentário em JavaDoc com mais de uma linha.
+*  teste teste teste.
+*/
 @ComponentScan(basePackageClasses = { TestController.class })
+/*
+* Exemplo básico de um comentário em JavaDoc com mais de uma linha.
+* teste teste teste.
+*/
 @EnableAutoConfiguration
+/*
+* Exemplo básico de um comentário em JavaDoc com mais de uma linha.
+* @return teste teste teste.
+*/
 public class TestServiceApplication {
 
-	private String bar = new String("bar");
 	public static void main(String[] args) {
 		SpringApplication.run(TestServiceApplication.class, args);
 		System.exit(0);
